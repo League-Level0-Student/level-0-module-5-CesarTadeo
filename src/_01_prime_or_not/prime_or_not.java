0311b9855c421a45f45e01b0fae4298ee5408bc6
League Student <league@iMac-21.attlocal.net>
2020-01-12 12:57:51 -0800

    prime or not , fibonacci(IP)

--- src/_01_prime_or_not/prime_or_not.java
+++ src/_01_prime_or_not/prime_or_not.java
@@ -7,16 +7,16 @@ public static void main(String[] args) {
 	String number = JOptionPane.showInputDialog("Give me any number, I will tell you if it is prime or composite");
 	int num = Integer.parseInt(number);
 	
-	for (int i = 1; i < num; i++) {
-		if (num%i == 1 || num%i == num) {
-			JOptionPane.showMessageDialog(null, "Your number is prime");
-			
-		}
-		else {
+	for (int i = 2; i < num; i++) {
+		if (num%i==0) {
 			JOptionPane.showMessageDialog(null, "Your number is not prime it is composite");
-			
+			System.exit(0);
 		}
+		
 	}
-	
+	 {
+		JOptionPane.showMessageDialog(null, "Your number is prime");
+		System.exit(0);
+	}
 }
-} 
+}
---
+++ src/_02_fibonacci/fibonacci.java
@@ -0,0 +1,58 @@
+package _02_fibonacci;
+
+import java.awt.FontFormatException;
+
+public class fibonacci {
+public static void main(String[] args) {
+	   	
+
+	for (int i = 0; i < 12; i++) {
+		
+		int x = 0;
+		int y = 1;
+		int sum = x+y;
+		x = y;
+		y = sum;		
+		System.out.println(x);		
+				
+				
+				
+				
+				
+	}
+	
+		
+
+	   
+	   
+	   
+	   
+	   
+	   
+
+	   
+	   	   
+	   
+	   
+
+	   
+	   
+	    
+
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
