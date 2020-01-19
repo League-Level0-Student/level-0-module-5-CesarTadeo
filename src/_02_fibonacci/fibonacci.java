93f8cda1609b774dff2357a8c53b17fda123904d
League Student <league@iMac-10.attlocal.net>
2020-01-19 12:55:57 -0800

    fibonacci, goofynames(IP)

--- src/_02_fibonacci/fibonacci.java
+++ src/_02_fibonacci/fibonacci.java
@@ -4,55 +4,21 @@ import java.awt.FontFormatException;
 
 public class fibonacci {
 public static void main(String[] args) {
-	   	
-
+	int x = 0;
+	int y = 1;  	
+	
 	for (int i = 0; i < 12; i++) {
 		
-		int x = 0;
-		int y = 1;
+	
 		int sum = x+y;
 		x = y;
 		y = sum;		
-		System.out.println(x);		
-				
-				
-				
-				
-				
-	}
-	
+		System.out.println(sum);		
+
 		
-
-	   
-	   
-	   
-	   
-	   
-	   
-
-	   
-	   	   
-	   
-	   
-
-	   
-	   
-	    
-
-}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+			
+	  
+	}  
+	}
+	}
 
--- src/_03_goofy_names/GoofyNames.java
+++ src/_03_goofy_names/GoofyNames.java
@@ -11,10 +11,11 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-
+		String name = JOptionPane.showInputDialog("Please tell me your name");
 		// 2. Print upper case name to the console using .toUpperCase()
 		//    Run your program to see that this works.
-
+		name = name.toUpperCase();
+		JOptionPane.showMessageDialog(null, name);
 		// 3. Loop through each character of the name (steps 4 - 6).
 		//    HINT: Use .length() to determine the number of characters in the String.
 
