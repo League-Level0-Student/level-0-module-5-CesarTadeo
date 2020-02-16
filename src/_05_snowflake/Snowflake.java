ff9f977ca872934cec301ebf3c96de13d9776a5f
League Student <league@iMac-21.attlocal.net>
2020-02-16 12:56:30 -0800

    guessing pi, for loop gauntlet
    snowflake isn't part of module 5 but it says it is

--- src/_05_snowflake/Snowflake.java
+++ src/_05_snowflake/Snowflake.java
@@ -6,6 +6,9 @@ package _05_snowflake;
 
 import java.awt.Color;
 
+import org.jointheleague.graphical.robot.Robot;
+
+
 
 public class Snowflake {
 	
@@ -13,47 +16,71 @@ public class Snowflake {
 
 		// #1. Make a new Robot
 
+		Robot bot = new Robot();
 
 		// #2. Set your robot’s position to x=300 and y=300
 
-
+		bot.setX(300);
+		bot.setY(300);
+		
 		// #3. Put the robot's pen down
 
+		
+		bot.penDown();
 
 		// #4. Set the robot’s speed to 5
 
-
+		bot.setSpeed(5);
+		
 		// #5. use a for loop below to do everything ( #6, #7, #8, #9, #12, #13, #14 ) 6 times (use i as the counter)
 
+		for (int i = 0; i < 6; i++) {
+			
+		
 			
 			// #6. Set the pen color to black
 
+		//bot.setPenColor(0, 0, 0);
 			
 			// #7. move the robot by 50
 
+		bot.move(50);
 			
 			// #8. turn the robot to the right by 60
 			
+		bot.turn(60);
 			
 			// #12. set the pen color to blue for even numbers of i
 
-			
+		if (i%2==0) {
+			bot.setPenColor(0, 0, 200);
+		}	
+		
 			// #13. set the pen color to red for all odd numbers of i
 			
-			
+		if (i%2==1) {
+			bot.setPenColor(200, 0, 0);
+		}
+		
 			// #9. Use a for loop to do everything (#10, #11) 6 times ( use j as a counter )
 			
+		for (int j = 0; j < 6; j++) {
+			
+		
 			
 					// #10. turn the robot to the left by 60
 		
-					
+		bot.turn(180);			
+		
 					// #11. move the robot by 50 (play the start button)
 					
-				
-			
+				bot.move(50);
+		}
 			// #14. set the pen color to black
 				
-		
-	}
+		bot.setPenColor(0, 0, 0);
 
+
+}		
+}
 }
---
+++ src/_06_for_loop_gauntlet/for_loop_gauntlet.java
@@ -0,0 +1,191 @@
+package _06_for_loop_gauntlet;
+
+public class for_loop_gauntlet {
+public static void main(String[] args) {
+	
+	//for (int i = 0; i < 101; i++) {
+	//	System.out.println(i);
+	//}
+	
+	//for (int i = 100; i >= 0; i--) {
+	//	System.out.println(i);
+	//}
+	
+	//for (int i = 0; i < 101; i++) {
+	//	if (i%2==0) {
+	//		System.out.println(i);
+	//	}		
+	//}
+			
+	//for (int i = 0; i < 100; i++) {
+	//	if (i%2==1) {
+	//		System.out.println(i);
+	//	}	
+	//}
+	
+	//for (int i = 1; i <= 500; i++) {
+	//	if (i%2==0) {
+	//		System.out.println(i+" is even");
+	//	}
+	//	else {
+	//		System.out.println(i+" is odd");
+	//	}
+	//}
+	
+	//for (int i = 0; i <= 777; i++) {
+	//	if (i%7==0) {
+	//		System.out.println(i);
+	//	}
+	//}
+	
+	
+	//for (int j = 2006; j <=2020 ; j++) {
+	//	System.out.println("In "+j+", I was "+(j-2006)+" years old");	
+	//}
+	
+	//for (int i = 0; i <= 2; i++) {
+	//for (int j = 0; j < 3; j++) {
+	//System.out.println(i+""+j);	
+	//}		
+	//}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
